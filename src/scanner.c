0822d9fb21c1aea58ae6c53b2de32fb101727ab3
Alex <ad.23970@hotmail.com>
2020-02-14 22:27:55 -0500

    tokenizer logic, whitespace handling

--- src/scanner.c
+++ src/scanner.c
@@ -4,20 +4,33 @@
 list tokenize(char * file_path){
     FILE * fp = fopen(file_path, "r");
     list * tokenList;
-
     token = null;
 
+    skipWhiteSpace(FILE * fp);
     while (fpeek(fp) != EOF){
         token = nextToken(file_path);
-        add(&tokenList, token;
+        if (1 == 1){ // TODO: Check that there was no error in reading token
+            add(&tokenList, token);
+        }
+        skipWhiteSpace(FILE * fp);
     }
 
-    return tokens
+    fclose(fp);
+    return tokenList;
 }
 
-token nextToken(char * file_path)
+token nextToken(FILE * fp)
 {
     
+    if (isalpha(fpeek(fp)))
+    {
+        //read an identifier or reserved word
+    }
+    else if (isdigit(fpeek(fp)))
+    {
+        //read a number
+    }
+
 }
 
 char fpeek(FILE * fp)
@@ -25,4 +38,11 @@ char fpeek(FILE * fp)
     char c = fgetc(fp);
     ungetc(c, fp);
     return c;
+}
+void skipWhiteSpace(FILE * fp){
+    char c;
+    do{
+        c = fgetc(fp);
+    } (while isspace(c) != 0); 
+    ungetc(c, fp);
 }
