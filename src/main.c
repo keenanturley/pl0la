6462209003ce2fc0cee70c4760e1d7fe0415e46d
Keenan Turley <keenan.turley@gmail.com>
2020-02-15 17:52:45 -0500

    Implement first draft of main

--- src/main.c
+++ src/main.c
@@ -1,5 +1,17 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include "util.h"
 
-int main(void) {
-    printf("PM/0 Lexical Analyzer\n");
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: ./pm0la (file-path)\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char *file_path = argv[1];
+
+    // Print out source program
+    printf("Source Program:\n");
+    print_file(file_path);
+
 }
