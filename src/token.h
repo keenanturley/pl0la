4184a0e1ea0e5d1f2011bc10b71b204813059719
Keenan Turley <keenan.turley@gmail.com>
2020-02-15 19:52:18 -0500

    Fix whitespace in token.h

--- src/token.h
+++ src/token.h
@@ -2,12 +2,12 @@
 #define TOKEN_H
 
 /* Token Types */
-typedef enum token_type { nulsym = 1, identsym, numbersym, plussym, minussym, 
-    multsym, slashsym, oddsym, eqsym, neqsym, lessym, leqsym, 
-    gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym, 
-    periodsym, becomessym, beginsym, endsym, ifsym, thensym, 
-    whilesym, dosym, callsym, constsym, varsym, procsym, writesym, 
-    readsym, elsesym 
+typedef enum token_type { nulsym = 1, identsym, numbersym, plussym, minussym,
+    multsym, slashsym, oddsym, eqsym, neqsym, lessym, leqsym,
+    gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
+    periodsym, becomessym, beginsym, endsym, ifsym, thensym,
+    whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
+    readsym, elsesym
 } token_type;
 
 /* Token structure */
