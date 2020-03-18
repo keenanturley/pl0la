400e9c440a253cc6312a70976f3c51be86bb4f67
Keenan Turley <keenan.turley@gmail.com>
2020-03-18 17:09:22 -0400

    Remove call, proc, and else from token_type
    - Explained in docs/project-3-notes.md

---
+++ docs/project-3-notes.md
@@ -0,0 +1,9 @@
+# Project 3 Modifications
+
+## Token types
+
+Since `call`, `proc`, and `else` are not included in the project 3 language specification, they are not allowed to be used.
+
+As per the professor, if we include references to these symbols anywhere in the source code of the project we will receive an automatic 0.
+
+Therefore, we have to modify the `token_type` enum to omit these.
--- src/token.h
+++ src/token.h
@@ -6,8 +6,8 @@ typedef enum token_type { nulsym = 1, identsym, numbersym, plussym, minussym,
     multsym, slashsym, oddsym, eqsym, neqsym, lessym, leqsym,
     gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
     periodsym, becomessym, beginsym, endsym, ifsym, thensym,
-    whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
-    readsym, elsesym
+    whilesym, dosym, constsym = 28, varsym, writesym = 31,
+    readsym
 } token_type;
 
 /* Token structure */
